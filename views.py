18c48c0faa15c8f290166c25ce6a3bb791521584
aparsaee <arp1051@yahoo.com>
2017-03-01 00:42:50 +0330

    First Django-Zarinpal Commit

---
+++ views.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Sample Django ZarinPal WebGate with SOAP
+
+__Author__ = 'Ahmadreza Parsaee'
+__Academy__ = 'SAMAR Web academy'
+__License__ = "GPL 2.0 http://www.gnu.org/licenses/gpl-2.0.html"
+
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from suds.client import Client
+
+
+MMERCHANT_ID = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'  # Required
+ZARINPAL_WEBSERVICE = 'https://www.zarinpal.com/pg/services/WebGate/wsdl'  # Required
+amount = 1000  # Amount will be based on Toman  Required
+description = u'توضیحات تراکنش تستی'  # Required
+email = 'user@userurl.ir'  # Optional
+mobile = '09123456789'  # Optional
+CallbackURL = 'http://127.0.0.1:8000/verify/'
+
+
+def send_request(request):
+    client = Client(ZARINPAL_WEBSERVICE)
+    result = client.service.PaymentRequest(MMERCHANT_ID,
+                                           amount,
+                                           description,
+                                           email,
+                                           mobile,
+                                           CallbackURL)
+    if result.Status == 100:
+        return redirect('https://www.zarinpal.com/pg/StartPay/' + result.Authority)
+    else:
+        return HttpResponse('Error')
+
+
+def verify(request):
+    client = Client(ZARINPAL_WEBSERVICE)
+    if request.GET.get('Status') == 'OK':
+        result = client.service.PaymentVerification(MMERCHANT_ID,
+                                                    request.GET['Authority'],
+                                                    amount)
+        if result.Status == 100:
+            return HttpResponse('Transaction success. RefID: ' + str(result.RefID))
+        elif result.Status == 101:
+            return HttpResponse('Transaction submitted : ' + str(result.Status))
+        else:
+            return HttpResponse('Transaction failed. Status: ' + str(result.Status))
+    else:
+        return HttpResponse('Transaction failed or canceled by user')

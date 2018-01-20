c59de4223df6048786a167d1b0f7e7db444f6e45
parsae <arp1053@yahoo.com>
2018-01-20 19:52:33 +0330

    Update views.py

--- views.py
+++ views.py
@@ -2,7 +2,7 @@
 
 # Sample Django ZarinPal WebGate with SOAP
 
-__Author__ = 'Ahmadreza Parsaee'
+__Author__ = 'Ahmadreza Parsae'
 __Academy__ = 'SAMAR Web academy'
 __License__ = "GPL 2.0 http://www.gnu.org/licenses/gpl-2.0.html"
 
